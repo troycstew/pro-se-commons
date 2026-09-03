74378f02090f13c54193f2b70aa01a4b3acdaf45
troycstew <troycstew@aol.com>
2026-09-03 13:34:12 -0400

    Create Home component for landing page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>Pro Se Commons</h1>
+      <p>P2P Marketplace for Redacted Legal Templates</p>
+      <div style={{ marginTop: "20px" }}>
+        <a href="/marketplace" style={{ padding: "10px 20px", background: "#0070f3", color: "#fff", textDecoration: "none", borderRadius: "5px" }}>
+          Browse Marketplace
+        </a>
+      </div>
+    </div>
+  );
+}
